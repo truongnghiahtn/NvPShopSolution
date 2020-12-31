efa191145d8f7f69b50c252dfc8c5b69b5b98aa2
Truong Nghia <Truong Nghia@DESKTOP-BBH7MEP>
2020-12-31 08:47:08 +0700

    Add project files.

---
+++ NvPShop.BackEnd/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NvPShop.BackEnd.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
